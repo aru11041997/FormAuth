ab09dd9ba3fb3d6de072d69782b9d8a3fa9990e4
Anand Uperpelli <anand_uperpelli@epam.com>
2020-03-19 18:21:27 +0530

    Form Auth

---
+++ FormAuth/FormAuth/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FormAuth.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection formCollection)
+        {
+            if (formCollection.Get("UserName")=="Anand" && formCollection.Get("Password") == "Anand")
+            {
+                FormsAuthentication.SetAuthCookie(formCollection.Get("UserName"), true);
+                return View("Success");
+            }
+
+                return View("Login");
+        }
+
+        [Authorize]
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
